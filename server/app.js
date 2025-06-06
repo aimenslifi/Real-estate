07386dce99e5b367ceeaf28bbd3440c5a005fd01
aimenslifi <slifiaimen27@gmail.com>
2025-06-06 03:13:22 +0100

    Delete server/app.js

--- server/app.js
+++
@@ -1,5 +0,0 @@
-// Simple server starter (paste exactly)
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Backend is working!'));
-app.listen(5000, () => console.log('Server running on port 5000'));
