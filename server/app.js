87660e554a390d624a438f93195ed575a01179e9
aimenslifi <slifiaimen27@gmail.com>
2025-06-06 03:08:59 +0100

    Delete server directory

--- server/app.js
+++
@@ -1,5 +0,0 @@
-// Backend Starter (safe test code)
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Backend works!'));
-app.listen(5000, () => console.log('Server is running'));
